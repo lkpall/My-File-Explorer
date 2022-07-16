2a40bddc8d7883cd00f18af8d848d282891f0b95
MATHEUS FELIPE DA SILVA PINHEIRO <pinheiro.felipe@academico.ifrn.edu.br>
2022-07-15 21:06:43 -0300

    Adicionando metodos de leitura e escrita de arquivos

--- main.py
+++ main.py
@@ -10,15 +10,13 @@ class MyFileExplorer:
         pass
 
     def get_file_attr(self, filename: str) -> Dict:
-        attrs = {
-            'name': filename,
-            'tamanho': os.path.getsize(filename),
-            'data_mod': date_format(os.path.getmtime(filename)),
-            'data_criacao': date_format(os.path.getctime(filename)),
-            'id': os.stat(filename).st_uid
-        }
+        id = os.stat(filename).st_uid[0],
+        name = filename[0],
+        tamanho = os.path.getsize(filename)[0],
+        data_criacao = date_format(os.path.getctime(filename))[0],
+        data_mod = date_format(os.path.getmtime(filename))
 
-        return attrs
+        return f"{id} {name} {tamanho} {data_criacao} {data_mod}"
 
     def list_items_path(self):
         items = os.listdir(self.get_path_current_dir())
@@ -32,8 +30,37 @@ class MyFileExplorer:
     def get_path_current_dir(self) -> str:
         return os.getcwd()
 
-    def create_folder(self, dir_name: str) -> None:
-        os.mkdir(self.join_path_url(dir_name))
+    def current_dir(self) -> None:
+        print(self.get_path_current_dir())
+
+    def create_folder(self, dirname: str) -> None:
+        os.mkdir(self.join_path_url(dirname))
+
+    def change_dir(self, dirname: str) -> None:
+        try:
+            os.chdir(self.join_path_url(dirname))
+        except IsADirectoryError:
+            print("Diretorio inexistente!")
+
+    def rename_dir(self, dirname, new_dirname):
+        source_name = self.join_path_url(dirname)
+        new_name = self.join_path_url(new_dirname)
+        try:
+            os.rename(source_name, new_name)
+        except IsADirectoryError:
+            print("Diretorio inexistente!")
+
+    def delete_folder(self, dirname: str) -> None:
+        dir_path = self.join_path_url(dirname)
+
+        if os.path.exists(dir_path):
+            # Verifica se a pasta está vazia
+            if len(os.listdir(dir_path)) == 0:
+                os.rmdir(dir_path)
+            else:
+                print("A pasta nao esta vazia!")
+        else:
+            print("O diretorio nao existe!")
 
     def create_file(self, filename: str) -> None:
         try:
@@ -73,24 +100,43 @@ class MyFileExplorer:
 
         shutil.copy(filename, new_filename)
 
+    def write_text(self, filename: str) -> None:
+        with open(self.join_path_url(filename), 'a') as f:
+            text = input("Digite aqui $> ")
+            f.write(text)
+            
+    def read_text(self, filename: str) -> None:
+        if os.path.exists(self.join_path_url(filename)):
+            with open(self.join_path_url(filename), 'r') as f:
+                print(f.readlines())
+        else:
+            print("Arquivo inexistente!")
+
 
 mfe = MyFileExplorer()
 
 dict_commands = {
     "createfolder": mfe.create_folder,
+    "deletefolder": mfe.delete_folder,
+    "cd": mfe.change_dir,
+    "rename": mfe.rename_dir,
     "createfile": mfe.create_file,
     "deletefile": mfe.delete_file,
     "movefile": mfe.move_file,
     "copyfile": mfe.copy_file,
-    "ls": mfe.list_items_path
+    "ls": mfe.list_items_path,
+    "nano": mfe.write_text,
+    "cat": mfe.read_text,
+    "pwd": mfe.current_dir
 }
 
 while True:
-    command = input("#> ")
+    current_dir = mfe.get_path_current_dir()
+    command = input(f"{current_dir} #> ")
     args = command.split()
     try:
         if len(args) > 1:
-            dict_commands[args[0]](args[1])
+            dict_commands[args[0]](*args[1:])
         else:
             dict_commands[args[0]]()
     except KeyError:
